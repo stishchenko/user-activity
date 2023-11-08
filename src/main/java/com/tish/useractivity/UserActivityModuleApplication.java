7f8bf8b39c4e21c3108b36db91003b2e2bf781f5
stishchenko <s.tischenko01@gmail.com>
2023-11-08 21:10:59 +0100

    Save all

---
+++ src/main/java/com/tish/useractivity/UserActivityModuleApplication.java
@@ -0,0 +1,13 @@
+package com.tish.useractivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tish/useractivity/UserActivityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tish.useractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
