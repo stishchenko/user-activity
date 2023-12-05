50fd766ea87a42e9baec136119d7cb6781c1ed84
stishchenko <s.tischenko01@gmail.com>
2023-12-05 04:27:10 +0100

    Understand how add charts

--- src/main/java/com/tish/controllers/LocationController.java
+++ src/main/java/com/tish/controllers/LocationController.java
@@ -1,18 +1,20 @@
 package com.tish.controllers;
 
+import com.tish.models.Settings;
 import com.tish.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@Controller
-@RestController
-@RequestMapping(path = "/location")
+@Controller
+//@RestController
+@RequestMapping(path = {"/location"})
 public class LocationController {
 
 	private final LocationService locationService;
@@ -25,47 +27,70 @@ public class LocationController {
 	@GetMapping(path = { "/country"})
 	public String getLocationCountryPage(Model model) {
 
+		model.addAttribute("type", "bar");
+		model.addAttribute("axis", "x");
+		/*model.addAttribute("values", Arrays.asList(10, 15, 12, 17, 30, 22));*/
+
+		model.addAttribute("labels", Arrays.asList("1", "2", "3", "4", "5", "6"));
+		model.addAttribute("userValues", Arrays.asList(10, 15, 17, 13, 20, 8));
+		model.addAttribute("visitValues", Arrays.asList(12, 17, 23, 12, 22, 14));
+		model.addAttribute("userPercent", Arrays.asList(10, 15, 17, 13, 20, 8));
+		model.addAttribute("visitPercent", Arrays.asList(12, 17, 23, 12, 22, 14));
+
+		model.addAttribute("settings", new Settings());
+
+		model.addAttribute("objectType", Arrays.asList("user", "visit"));
+		model.addAttribute("dataType", Arrays.asList("value", "percent"));
+
 		return "country-statistics";
 	}
 
 	@GetMapping(path = { "/city"})
 	public String getLocationCityPage(Model model) {
 
+		model.addAttribute("type", "bar");
+		model.addAttribute("axis", "x");
+		model.addAttribute("values", Arrays.asList(10, 15, 12, 17, 30, 22));
+
+		model.addAttribute("labels", Arrays.asList("1", "2", "3", "4", "5", "6"));
+		model.addAttribute("settings", new Settings());
+
 		return "city-statistics";
 	}
 
 	@PostMapping(path = {"/country"})
-	public Map<Object, Object> getCountries(/*@RequestBody(required = false) String graphicType,
+	public String getCountries(/*@RequestBody(required = false) String graphicType,
 											@RequestBody String statisticsType,
 											@RequestBody String dataType,
 											@RequestBody String webApp,
 											@RequestBody String fromDate, @RequestBody String toDate*/
-			@RequestBody Map<String, String> params/*, @ModelAttribute("settings") Settings settings, Model model*/) {
+			/*@RequestBody Map<String, String> params,*/ @ModelAttribute("settings") Settings settings, Model model) {
 		// dataType  = value, percent or value+percent
-		List<Map<String, Double>> userMapList = locationService.getCountriesStatistics("user", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
+		/*List<Map<String, Double>> userMapList = locationService.getCountriesStatistics("user", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
 		List<Map<String, Double>> visitMapList = locationService.getCountriesStatistics("visit", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
 		Map<Object, Object> allMap = new HashMap<>();
 		allMap.put("users", userMapList);
-		allMap.put("visits", visitMapList);
+		allMap.put("visits", visitMapList);*/
+		model.addAttribute("settings", new Settings());
 
-		return allMap;
+		return "country-statistics";
 	}
 
 	@PostMapping(path = {"/city"})
-	public Map<Object, Object> getCities(/*@RequestBody(required = false) String graphicType,
+	public String getCities(/*@RequestBody(required = false) String graphicType,
 										 @RequestBody String statisticsType,
 										 @RequestBody String dataType,
 										 @RequestBody String webApp,
 										 @RequestBody String fromDate, @RequestBody String toDate*/
-			@RequestBody Map<String, String> params/*, @ModelAttribute("settings") Settings settings, Model model*/) {
+			/*@RequestBody Map<String, String> params,*/ @ModelAttribute("settings") Settings settings, Model model) {
 		// dataType  = value, percent or value+percent
-		List<Map<String, Double>> userMapList = locationService.getCitiesStatistics("user", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
+		/*List<Map<String, Double>> userMapList = locationService.getCitiesStatistics("user", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
 		List<Map<String, Double>> visitMapList = locationService.getCitiesStatistics("visit", params.get("dataType"), params.get("fromDate"), params.get("toDate"), params.get("webApp"));
 
 		Map<Object, Object> allMap = new HashMap<>();
 		allMap.put("users", userMapList);
-		allMap.put("visits", visitMapList);
+		allMap.put("visits", visitMapList);*/
 
-		return allMap;
+		return "city-statistics";
 	}
 }
--- src/main/java/com/tish/models/Settings.java
+++ src/main/java/com/tish/models/Settings.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Data
@@ -14,9 +15,9 @@ import java.util.List;
 public class Settings {
 	private String chartType;
 	private String periodType = "month";
-	private List<String> dataTypes;
-	private List<String> statisticObjectType;
-	private String startDate = "2021-01-01";
+	private List<String> dataTypes = Arrays.asList("value", "percent");
+	private List<String> statisticObjectType = Arrays.asList("user", "visit");;
+	private String startDate = "2022-01-01";
 	private String endDate = "2023-12-31";
 	private String webApp;
 }
