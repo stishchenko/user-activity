246cd0543ecdfa71de93838595a17eedab8a4b0a
stishchenko <s.tischenko01@gmail.com>
2023-12-13 19:55:59 +0100

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -2,7 +2,7 @@ create database activity_module;
 
 use activity_module;
 
-create table accounts
+create table if not exists accounts
 (
     id         int auto_increment
         primary key,
@@ -16,7 +16,7 @@ create table accounts
 insert into accounts(login, password, user_name)
 values ('login', 'login', 'login');
 
-create table devices
+create table if not exists devices
 (
     device_id     varchar(255) not null
         primary key,
@@ -28,7 +28,7 @@ create table devices
 )
     collate = utf8mb4_unicode_ci;
 
-create table locations
+create table if not exists locations
 (
     id            int auto_increment
         primary key,
@@ -39,7 +39,7 @@ create table locations
 )
     collate = utf8mb4_vietnamese_ci;
 
-create table users
+create table if not exists users
 (
     id            int auto_increment
         primary key,
@@ -54,7 +54,7 @@ create table users
 )
     collate = utf8mb4_unicode_ci;
 
-create table visits
+create table if not exists visits
 (
     id            int auto_increment
         primary key,
@@ -74,64 +74,3 @@ create table visits
         foreign key (user_id) references users (user_id)
 )
     collate = utf8mb4_unicode_ci;
-
-
-
-create table devices
-(
-    device_id     varchar(255) not null
-        primary key,
-    type          varchar(255) null,
-    os_platform   varchar(255) null,
-    browser       varchar(255) null,
-    web_app       varchar(255) null,
-    creation_date varchar(255) null
-)
-    collate = utf8mb4_unicode_ci;
-
-create table locations
-(
-    id            int auto_increment
-        primary key,
-    country       varchar(255) null,
-    city          varchar(255) null,
-    creation_date varchar(255) null
-)
-    collate = utf8mb4_vietnamese_ci;
-
-create table users
-(
-    id            int auto_increment
-        primary key,
-    user_id       varchar(255) not null,
-    device_id     varchar(255) not null,
-    web_app       varchar(255) null,
-    creation_date varchar(255) null,
-    constraint user_id
-        unique (user_id),
-    constraint fk_device_id_u
-        foreign key (device_id) references devices (device_id)
-)
-    collate = utf8mb4_unicode_ci;
-
-create table visits
-(
-    id            int auto_increment
-        primary key,
-    user_id       varchar(255) default '0'  not null,
-    device_id     varchar(255) default 'no' not null,
-    location_id   int          default 0    not null,
-    web_app       varchar(255)              null,
-    page          varchar(255)              null,
-    visit_time    double                    null,
-    target_action int          default 0    null,
-    creation_date varchar(255)              null,
-    constraint fk_device_id
-        foreign key (device_id) references devices (device_id),
-    constraint fk_location_id
-        foreign key (location_id) references locations (id),
-    constraint fk_user_id
-        foreign key (user_id) references users (user_id)
-)
-    collate = utf8mb4_unicode_ci;
-
